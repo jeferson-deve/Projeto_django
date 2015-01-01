612a560928bfbc90ca12d98a4c0dcee52efef042
Jeferson <mp_bra_nco@hotmail.com>
2014-12-31 22:44:11 -0200

    .

--- meusite/meublog/models.py
+++ meusite/meublog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 class Artigo(models.Model):
+    class Meta:
+        ordering = ('-publicacao',)
     titulo = models.CharField(max_length=100)
     conteudo = models.TextField()
     publicacao = models.DateTimeField()
--- meusite/meusite/urls.py
+++ meusite/meusite/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic import ArchiveIndexView
+from meublog.models import Artigo
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,4 +17,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', ArchiveIndexView.as_view(model=Artigo, date_field='publicacao'))
 )
