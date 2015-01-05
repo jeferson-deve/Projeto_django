15e435e39b75ccf854bf20ac36cc393f0d41e4fa
Jeferson <mp_bra_nco@hotmail.com>
2015-01-04 22:14:37 -0200

    .

--- meu_blog/blog/models.py
+++ meu_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    publicacao = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.titulo
